901ccff191233aa67ee690717a1d56eab50df01c
vicentelyrio <vicentelyrio@gmail.com>
2024-06-01 18:00:10 -0300

    launcher layout and tokens

--- src/modules/Launcher/Launcher.ts
+++ src/modules/Launcher/Launcher.ts
@@ -6,20 +6,22 @@ const WINDOW_NAME = 'applauncher'
 
 const AppItem = (app: Application) => {
   return Widget.Button({
+    className: 'launcher__item',
     on_clicked: () => {
       App.closeWindow(WINDOW_NAME)
       app.launch()
     },
     attribute: { app },
     child: Widget.Box({
-      spacing: 8,
+      className: 'launcher__item__container',
       children: [
         Widget.Icon({
+          className: 'launcher__item__icon',
           icon: app.icon_name || '',
           size: 42,
         }),
         Widget.Label({
-          class_name: 'title',
+          className: 'launcher__item__label',
           label: app.name,
           xalign: 0,
           vpack: 'center',
@@ -30,15 +32,16 @@ const AppItem = (app: Application) => {
   })
 }
 
-const Applauncher = ({ width = 500, height = 500, spacing = 12 }) => {
+const Applauncher = () => {
   // list of application buttons
   let applications = query('').map(AppItem)
 
   // container holding the buttons
   const list = Widget.Box({
+    className: 'launcher__list',
     vertical: true,
     children: applications,
-    spacing,
+    spacing: 12,
   })
 
   // repopulate the box, so the most frequent apps are on top of the list
@@ -48,14 +51,15 @@ const Applauncher = ({ width = 500, height = 500, spacing = 12 }) => {
   }
 
   // search entry
-  const entry = Widget.Entry({
+  const search = Widget.Entry({
+    className: 'launcher__search',
     hexpand: true,
-    css: `margin-bottom: ${spacing}px;`,
 
     // to launch the first item on Enter
     on_accept: () => {
       // make sure we only consider visible (searched for) applications
-      const results = applications.filter((item) => item.visible);
+      const results = applications.filter((item) => item.visible)
+
       if (results[0]) {
         App.toggleWindow(WINDOW_NAME)
         results[0].attribute.app.launch()
@@ -69,15 +73,15 @@ const Applauncher = ({ width = 500, height = 500, spacing = 12 }) => {
   })
 
   return Widget.Box({
+    className: 'launcher__box',
     vertical: true,
-    css: `margin: ${spacing * 2}px;`,
     children: [
-      entry,
+      search,
 
       // wrap the list in a scrollable
       Widget.Scrollable({
+        className: 'launcher__scroll',
         hscroll: 'never',
-        css: `min-width: ${width}px;` + `min-height: ${height}px;`,
         child: list,
       }),
     ],
@@ -88,23 +92,20 @@ const Applauncher = ({ width = 500, height = 500, spacing = 12 }) => {
       // when the applauncher shows up
       if (visible) {
         repopulate()
-        entry.text = ''
-        entry.grab_focus()
+        search.text = ''
+        search.grab_focus()
       }
     }),
   })
 }
 
 export const Launcher = Widget.Window({
+  className: 'launcher',
   name: WINDOW_NAME,
   setup: self => self.keybind('Escape', () => {
     App.closeWindow(WINDOW_NAME)
   }),
   visible: false,
   keymode: 'exclusive',
-  child: Applauncher({
-    width: 500,
-    height: 500,
-    spacing: 12,
-  }),
+  child: Applauncher(),
 })
